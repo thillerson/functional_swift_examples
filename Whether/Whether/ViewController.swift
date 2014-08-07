10d1957ca950604f84c78c66b3b07ef8feb4d78a
Tony Hillerson <tony.hillerson@gmail.com>
2014-08-07 15:56:35 -0600

    Fresh out of Xcode

---
+++ Whether/Whether/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whether
+//
+//  Created by Tony Hillerson on 8/7/14.
+//  Copyright (c) 2014 Tack Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
