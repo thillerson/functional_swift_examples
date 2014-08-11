0b5aac8be97dc9569d2acbfa04c0b51415383c6c
Tony Hillerson <tony.hillerson@gmail.com>
2014-08-11 15:53:10 -0600

    Displaying weather results in cells

--- Whether/Whether/ViewController.swift
+++ Whether/Whether/ViewController.swift
@@ -8,18 +8,97 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+
+  @IBOutlet weak var collectionView: UICollectionView!
+  
+  private var weather:Array<(String,String)>?
+  
+  let zips = [
+    "80003",
+    "80216",
+    "80303",
+    "80111",
+    "80205",
+  ]
+ 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    loadWeather()
+  }
+  
+  func loadWeather() {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+      
+      let results = self.zips
+        .map { zip -> (String, NSURLRequest) in
+          return (zip, self.zipToRequest(zip))
+        }
+        .map { zip, request -> (String, NSDictionary) in
+          return (zip, self.requestToJSON(request))
+        }
+        .map { zip, json -> (String, String) in
+          return (zip, self.jsonToWeatherDescription(json))
+        }
+      
+      dispatch_async(dispatch_get_main_queue(), { self.displayWeather(results) })
+    }
+  }
+  
+  func displayWeather(results:Array<(String, String)>) {
+    weather = results
+    collectionView.reloadData()
+  }
+  
+  func zipToRequest(zip:String) -> NSURLRequest {
+    let earl = NSURL(string: "http://query.yahooapis.com/v1/public/yql?format=json&q=select%20item%20from%20weather.forecast%20where%20location%3D%27\(zip)%27");
+    return NSURLRequest(URL: earl)
+  }
+  
+  func requestToJSON(request:NSURLRequest) -> NSDictionary {
+    var response: NSURLResponse?
+    var error: NSErrorPointer = nil
+    let data = NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: error)
+    let json = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: error) as NSDictionary
+    return json
+  }
+  
+  func jsonToWeatherDescription(json:NSDictionary) -> String {
+    let query = json["query"] as NSDictionary
+    let results = query["results"] as NSDictionary
+    let channel = results["channel"] as NSDictionary
+    let item = channel["item"] as NSDictionary
+    let condition = item["condition"] as NSDictionary
+    let temp = condition["temp"] as String
+    let description = condition["text"] as String
+    let weatherText = "\(temp) and \(description)"
+    
+    return weatherText
+  }
+  
+  //MARK: CollectionView Stuff
+  
+  func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int {
+    if let data = weather {
+      return data.count
+    } else {
+      return 0
+    }
+  }
+  
+  func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell! {
+    let (zip, description) = weather![indexPath.row]
+    
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier("weather_cell", forIndexPath: indexPath) as WeatherCell
+    cell.zip!.text = "\(zip)"
+    cell.text!.text = "\(description)"
+    return cell
+  }
+  
+  func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: UICollectionViewLayout!, sizeForItemAtIndexPath indexPath: NSIndexPath!) -> CGSize {
+    return CGSize(width: collectionView.frame.size.width, height: 55)
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
+  
 }
 
---
+++ Whether/Whether/WeatherCell.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCell.swift
+//  Whether
+//
+//  Created by Tony Hillerson on 8/11/14.
+//  Copyright (c) 2014 Tack Mobile. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherCell : UICollectionViewCell {
+  
+  @IBOutlet weak var text:UILabel?
+  @IBOutlet weak var zip:UILabel?
+  
+}
